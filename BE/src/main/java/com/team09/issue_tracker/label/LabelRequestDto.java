da12a62824894ce4166197d9b0c232f565eeeb6e
mybloom <baejungeun11@gmail.com>
2022-06-15 09:57:51 +0900

    feat: Label, Milestone 용 RequestDto 생성 (bugpigg/issue-tracker#1)

---
+++ BE/src/main/java/com/team09/issue_tracker/label/LabelRequestDto.java
@@ -0,0 +1,15 @@
+package com.team09.issue_tracker.label;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LabelRequestDto {
+
+	private String title;
+	private String description;
+	private String backgroundColor;
+	private boolean isDarkMode;
+
+}
---
+++ BE/src/main/java/com/team09/issue_tracker/milestone/MilestoneRequestDto.java
@@ -0,0 +1,15 @@
+package com.team09.issue_tracker.milestone;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MilestoneRequestDto {
+
+	private String title;
+	private String description;
+	private LocalDateTime completedAt;
+
+}
