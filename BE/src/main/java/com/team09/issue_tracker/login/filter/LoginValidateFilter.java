010f74ce59fe7b0b575aff9ea7cb1a1d0f06b3f8
bugpigg <bugpigg@gmail.com>
2022-06-21 18:38:49 +0900

    feat: LoginValidateFilter 에 토큰 기반으로 멤버 ID 추출 및 응답 헤더에 추가 기능 구현
    - LoginValidateFilter 에서 요청 헤더의 JWT 토큰을 기반으로 userId를 추출합니다. 그리고 MemberService 클래스의 findIdFromUserId() 함수를 통해 멤버 id를 구하고 이를 응답 헤더에 추가합니다. 이는 로그인이 요구되는 각 요청들에 대하여, 어떤 멤버가 보낸 요청인지 비즈니스 로직에서 식별할 필요성이 있기에 추가하였습니다. 필터에서 공통적으로 처리해주어 각 비즈니스 로직에서 중복될 수 있는 코드를 최소화 하였습니다.
    - Request 에 추가하는게 적절하다고 판단하나, 구현상 어려워 Response 에 추가하였습니다.

--- BE/src/main/java/com/team09/issue_tracker/login/filter/LoginValidateFilter.java
+++ BE/src/main/java/com/team09/issue_tracker/login/filter/LoginValidateFilter.java
@@ -1,6 +1,9 @@
 package com.team09.issue_tracker.login.filter;
 
 import com.team09.issue_tracker.login.jwt.JwtTokenProvider;
+import com.team09.issue_tracker.member.Member;
+import com.team09.issue_tracker.member.MemberRepository;
+import com.team09.issue_tracker.member.MemberService;
 import java.io.IOException;
 import java.util.Optional;
 import javax.servlet.FilterChain;
@@ -19,21 +22,21 @@ public class LoginValidateFilter extends OncePerRequestFilter {
 	private static final String HEADER_AUTHORIZATION = "Authorization";
 	private static final String TOKEN_PREFIX = "Bearer ";
 	private static final String[] VALID_FREE_URLS = {"/login/oauth/*"};
-
 	private final JwtTokenProvider tokenProvider;
+	private final MemberService memberService;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
 
 		String requestUrl = request.getRequestURI();
-
 		if (isValidRequiredUrl(requestUrl)) {
 			log.info("로그인 인증 유무 확인 로직 실행 ---> {}", requestUrl);
 			String accessToken = getAccessToken(request);
 			if (!tokenProvider.validateToken(accessToken)) {
 				throw new RuntimeException("유효하지 않은 JWT 토큰입니다!!");
 			}
+			addMemberIdToResponseHeader(accessToken, response);
 		}
 		filterChain.doFilter(request, response);
 	}
@@ -45,7 +48,15 @@ public class LoginValidateFilter extends OncePerRequestFilter {
 	private String getAccessToken(HttpServletRequest request) {
 		String header = Optional.ofNullable(request.getHeader(HEADER_AUTHORIZATION))
 			.orElseThrow(() -> new RuntimeException("JWT 토큰이 필요합니다!!"));
-
 		return header.substring(TOKEN_PREFIX.length());
+	}
+
+	private void addMemberIdToResponseHeader(String accessToken, HttpServletResponse response) {
+		String userId = (String) tokenProvider.parseClaims(accessToken).get("id");
+		log.info("유저 아이디 정보---> {}", userId);
+
+		Long id = memberService.findIdFromUserId(userId);
+
+		response.addHeader("userId", String.valueOf(id));
 	}
 }
