8fb5d72f2bc5f3dd55af5c58eaf70d0067fd9e24
mybloom <baejungeun11@gmail.com>
2022-06-25 08:06:11 +0900

    feat: Issue와 같은 도메인인 IssueLabel의 Repository서 이미 선택한/선택할 수 있는 레이블 목록 조회 쿼리 JPQL로 작성   (bugpigg/issue-tracker#24)
    - Lable과 IssueLabel이 조인되어 select 절의 projection이 2개 테이블의 컬럼으로 조합되어 있다.
    - 쿼리 결과값을 Tuple Interface를 사용하여 매핑

--- BE/src/main/java/com/team09/issue_tracker/issue/IssueLabelRepository.java
+++ BE/src/main/java/com/team09/issue_tracker/issue/IssueLabelRepository.java
@@ -1,8 +1,23 @@
 package com.team09.issue_tracker.issue;
 
 import com.team09.issue_tracker.issue.domain.IssueLabel;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 public interface IssueLabelRepository extends JpaRepository<IssueLabel, Long> {
+
+	@Query("select l.id as labelId, l.title as title, l.backgroundColor as backgroundColor, "
+		+ "l.isDarkMode as darkMode, "
+		+ "l.memberId as memberId, i.issue.id as issueId "
+		+ "from IssueLabel i "
+		+ "right join i.label l "
+		+ "where i.label.memberId = :memberId "
+		+ "and i.issue.id = :issueId "
+		+ "or (i.label.memberId = :memberId and i.issue.id is null)")
+	List<IEditableLabel> findBySelectable(@Param("issueId") Long issueId,
+		@Param("memberId") Long memberId);
 
 }
