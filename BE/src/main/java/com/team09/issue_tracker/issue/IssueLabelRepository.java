6a22b88ba962373952786722898398ae6f892e09
mybloom <baejungeun11@gmail.com>
2022-06-25 21:03:28 +0900

    feat: IssueLabelRepository 에 findByIssueAndLabel() 쿼리메서드 추가  (bugpigg/issue-tracker#24)

--- BE/src/main/java/com/team09/issue_tracker/issue/IssueLabelRepository.java
+++ BE/src/main/java/com/team09/issue_tracker/issue/IssueLabelRepository.java
@@ -1,7 +1,10 @@
 package com.team09.issue_tracker.issue;
 
+import com.team09.issue_tracker.issue.domain.Issue;
 import com.team09.issue_tracker.issue.domain.IssueLabel;
+import com.team09.issue_tracker.label.Label;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,4 +23,5 @@ public interface IssueLabelRepository extends JpaRepository<IssueLabel, Long> {
 	List<ISelectableLabel> findBySelectable(@Param("issueId") Long issueId,
 		@Param("memberId") Long memberId);
 
+	Optional<IssueLabel> findByIssueAndLabel(Issue issue, Label label);
 }
